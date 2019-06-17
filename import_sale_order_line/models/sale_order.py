06be96408324a87f2327cbaa3adb8874f3dbe4ca
许刚 <xugang@sinorail.com>
2019-06-17 13:54:19 +0800

    添加销售订单明细导入

---
+++ import_sale_order_line/models/sale_order.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+import time, datetime
+import math
+from collections import defaultdict, OrderedDict
+from operator import itemgetter
+from itertools import groupby
+from odoo.tools.misc import DEFAULT_SERVER_DATE_FORMAT as SD
+from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT as ST
+from odoo import models, fields, api, _, tools, SUPERUSER_ID
+import odoo.addons.decimal_precision as dp
+from odoo.tools import float_compare, float_round, float_is_zero
+from odoo.exceptions import UserError, ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def action_open_import_sale_order_line_wizard(self):
+        """
+
+        :return:
+        """
+        self.ensure_one()
+        self._cr.commit()
+        context = {}
+        context.update(active_model=self._name,
+                       active_ids=self.ids,
+                       active_id=self.id,
+                       )
+        self.env.context = context
+        wizard_id = self.env['import.sale.order.line.wizard'].create(dict(master_id=self.id))
+        return wizard_id.wizard_view()
