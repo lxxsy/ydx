a88b381ea5a36fbe27c5cc399b4f2aceabc96e68
username <1315034180@qq.com>
2019-09-29 08:48:14 +0800

    领料单明细代码规范修改

--- ydx_mrp/models/stock_move.py
+++ ydx_mrp/models/stock_move.py
@@ -4,13 +4,13 @@ from odoo import models, fields
 class YdxStockMove(models.Model):
 	_inherit = "stock.move"
 
-	Order_number = fields.Char(strint="Order_number")
-	The_child_orders = fields.Char(int="The_child_orders")
-	Base_material = fields.Char(string="Base material")
-	Plane_materiel = fields.Char(string="Plane materiel")
-	Thickness = fields.Float(string="Thickness")
-	Length = fields.Float(string="Length")
-	Breadth = fields.Float(string="Breadth")
+	order_number = fields.Char(strint="Order number")
+	the_child_orders = fields.Char(int="The child orders")
+	base_material = fields.Char(string="Base material")
+	plane_materiel = fields.Char(string="Plane materiel")
+	thickness = fields.Float(string="Thickness")
+	length = fields.Float(string="Length")
+	breadth = fields.Float(string="Breadth")
 
 
 
